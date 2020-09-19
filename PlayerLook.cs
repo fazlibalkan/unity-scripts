2392114fc56331e156722931c5b1ebef7eac6999
fazlibalkan <53044560+fazlibalkan@users.noreply.github.com>
2020-09-20 00:59:30 +0300

    Add files via upload

---
+++ PlayerLook.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    float mouseX;
+    float mouseY;
+
+    public float mouseSensitivity = 120f;
+
+    public Transform playerBody;
+
+    private float xRotation = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+
+
+
+    }
+}
---
+++ Wand.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+
+public class Wand : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public ParticleSystem muzzleFlash;
+
+    public Camera fpsCam;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+
+            Shoot();
+
+        }
+
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hitPoint;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hitPoint, range))
+        {
+            Debug.Log(hitPoint.transform.name);
+
+            Target target = hitPoint.transform.GetComponent<Target>();
+
+            if (target != null) {
+                target.TakeDamage(damage);
+            }
+
+        }
+
+    }
+}
